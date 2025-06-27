9e14e97a4166328983ac4067a0cfba2dc8ba09f0
rohan <brohan275@gmail.com>
2025-06-27 21:46:29 +0530

    Final Commit

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,3 +1,40 @@
 SELECT *
 FROM employee_salary
 WHERE salary >=50000;
+
+CREATE PROCEDURE large_salaries()
+SELECT *
+FROM employee_salary
+WHERE salary >=50000;
+
+CALL large_salaries();
+
+DELIMITER $$
+CREATE PROCEDURE large_salaries4()
+BEGIN
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 50000;
+	SELECT *
+	FROM employee_salary
+	WHERE salary >= 100000;
+END $$
+DELIMITER ;
+
+CALL large_salaries2();
+CALL large_salaries4();
+
+-- parameter
+DELIMITER $$
+CREATE PROCEDURE large_salaries2(p_employee_id INT)
+BEGIN
+	SELECT *
+	FROM employee_salary
+    WHERE employee_id = p_employee_id;
+END $$
+DELIMITER ;
+
+CALL large_salaries2(1);
+
+SELECT *
+FROM employee_salary;
